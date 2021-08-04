6e1dba2bed70bcac0ee16d46043e233427e39ef9
malv <melissa.gouveia@gft.com>
2021-08-04 09:25:43 -0300

    Correcao no database.

--- infraestrutura/database/conexao.js
+++ infraestrutura/database/conexao.js
@@ -4,7 +4,7 @@ const conexao = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'mysqlmel4542!!',
+    password: '',
     database: 'agenda_petshop'
 })
 
